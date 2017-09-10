2bd5b921037e52401a85ec0a4b95013a1db2b095
Prithwiraj Nath <prithwiraj.nath@purpleyo.com>
2017-09-11 01:46:34 +0530

    Add on click listener to logo

--- app/src/main/java/prithwimig16/visitnortheast/activities/HomeActivity.java
+++ app/src/main/java/prithwimig16/visitnortheast/activities/HomeActivity.java
@@ -6,28 +6,55 @@ import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import prithwimig16.visitnortheast.R;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView tvSigInLater;
+    ImageView loginManual, loginwithFb, loginWithGoogle;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        init();
+    }
+
+    private void init() {
+        this.loginManual = (ImageView) findViewById(R.id.img_login_logo);
+        this.loginManual.setOnClickListener(this);
+
+        this.loginwithFb = (ImageView) findViewById(R.id.img_fb_logo);
+        this.loginwithFb.setOnClickListener(this);
+
+        this.loginWithGoogle = (ImageView) findViewById(R.id.img_g_logo);
+        this.loginWithGoogle.setOnClickListener(this);
+
         this.tvSigInLater = (TextView) findViewById(R.id.tv_signin_ltr);
-        this.tvSigInLater.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(HomeActivity.this, CityListActivity.class);
-                startActivity(i);
-            }
-        });
+        this.tvSigInLater.setOnClickListener(this);
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.img_login_logo:
+                break;
+
+            case R.id.img_fb_logo:
+                break;
+
+            case R.id.img_g_logo:
+                break;
+
+            case R.id.tv_signin_ltr:
+                Intent i = new Intent(HomeActivity.this, CityListActivity.class);
+                startActivity(i);
+                break;
+        }
+    }
 }
