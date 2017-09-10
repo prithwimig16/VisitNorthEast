6591b0beed1a52204676fa7fb72d514190e793cd
Prithwiraj Nath <prithwiraj.nath@purpleyo.com>
2017-09-10 18:30:07 +0530

    comleted upto cityList

--- app/src/main/java/prithwimig16/visitnortheast/activities/CityListActivity.java
+++ app/src/main/java/prithwimig16/visitnortheast/activities/CityListActivity.java
@@ -2,24 +2,35 @@ package prithwimig16.visitnortheast.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.GridView;
 
 import prithwimig16.visitnortheast.R;
+import prithwimig16.visitnortheast.adapter.CityListAdapter;
 
-public class CityListActivity extends AppCompatActivity {
+public class CityListActivity extends AppCompatActivity implements View.OnClickListener {
     GridView gridView;
+    CityListAdapter adapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city_list);
         init();
+        this.gridView.setAdapter(this.adapter);
     }
 
     private void init() {
         this.gridView = (GridView) findViewById(R.id.gridview_city_activity);
+        this.adapter = new CityListAdapter(this, this, R.layout.single_view_city_list);
 
     }
 
+    @Override
+    public void onClick(View v) {
+
+    }
 
 }
--- app/src/main/java/prithwimig16/visitnortheast/adapter/CityListAdapter.java
+++ app/src/main/java/prithwimig16/visitnortheast/adapter/CityListAdapter.java
@@ -1,9 +1,84 @@
 package prithwimig16.visitnortheast.adapter;
 
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import prithwimig16.visitnortheast.R;
+import prithwimig16.visitnortheast.activities.CityListActivity;
+
 /**
  * Created by Prithwi on 10/09/17.
  */
 
-public class CityListAdapter {
+public class CityListAdapter extends ArrayAdapter {
+    Context context;
+    int layoutResourceId;
+    CityListActivity cityListActivity;
+    private String[] placeName = new String[]{"  Assam  ", "  Tripura  ", "  Manipur  ", "  Mizoram  ", "  Sikkim  ", "  Arunachal  ", "  Meghalaya  "};
+    private int[] imageArray = new int[]{
+            R.drawable.assam, R.drawable.tripura, R.drawable.manipur, R.drawable.mizoram, R.drawable.sikkim, R.drawable.arunachal,
+            R.drawable.shillong,
+
+    };
+
+
+    public CityListAdapter(@NonNull Context context, CityListActivity cityListActivity, @LayoutRes int layoutResourceId) {
+
+        super(context, layoutResourceId);
+
+        this.layoutResourceId = layoutResourceId;
+        this.context = context;
+        this.cityListActivity = cityListActivity;
+    }
+
+    @Override
+    public int getCount() {
+        return this.imageArray.length;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+        RecordHolder holder = null;
+        if (row == null) {
+            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+            row = inflater.inflate(layoutResourceId, parent, false);
+            holder = new RecordHolder();
+            holder.txtTitle = (TextView) row.findViewById(R.id.tv_city_name_city_list);
+            holder.imageItem = (ImageView) row.findViewById(R.id.item_image);
+
+            row.setTag(holder);
+        } else {
+            holder = (RecordHolder) row.getTag();
+        }
+
+        holder.imageItem.setTag("" + position);
+
+        holder.txtTitle.setText(this.placeName[position]);
+        try {
+            Picasso.with(context).load(this.imageArray[position]).into(holder.imageItem);
+        } catch (Exception e) {
+            // holder.propimage.setImageResource(R.drawable.imagenotfound);
+            holder.imageItem.setImageResource(R.drawable.noimage);
+        }
+
+
+        return row;
+    }
+
+    static class RecordHolder {
+        TextView txtTitle;
+        ImageView imageItem;
+    }
 
 }
